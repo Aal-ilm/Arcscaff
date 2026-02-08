833275876e68e18fd012776b3c91028ffc7cfc31
aal-ilm <cortex.aam@proton.me>
2026-02-08 02:44:24 -0500

    updated README

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the script directory
+cd /d C:\projects\myproject
+
+REM Run Python on __init__.py
+python __init__.py
+
+REM Pause so you can see output (optional)
+pause
